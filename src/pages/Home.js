0690cee3b81f2986f66b99c062cc9e98939094ca
valdeilson <vautronica@gmail.com>
2019-06-23 23:35:33 -0300

    estilizando o home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,71 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import {
-    View,
-    StyleSheet,
-    TouchableOpacity,
-    Text,
-    TextInput,
-    
-} from 'react-native';
-
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  TextInput
+} from "react-native";
+import logo from "../assets/logo.png";
 export default class Home extends Component {
-    static navigationOptions = {
-        headerTitle: 'Biblioteca IFMA - Timon'
-    };
+  static navigationOptions = {
+    headerTitle: "Biblioteca IFMA - Timon"
+  };
 
-    state = {
-        description: '',
-    };
+  state = {
+    description: ""
+  };
 
-    render() {
-        return(
-            <View style={styles.container}>
-                <TextInput 
-                    style={styles.input}
-                    autoCorrect={false}
-                    autoCapitalize='none'
-                    placeholder='Digite os termos da Pesquisa'
-                    placeholderTextColor='#999'
-                    value={this.state.description}
-                    onChange={description => this.setState({ description })}
-                />
-                <TouchableOpacity style={styles.searchButton} onPress={() => {}}>
-                    <Text style={styles.searchButtonText}>Buscar</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Digite os termos da Pesquisa"
+          placeholderTextColor="#999"
+          value={this.state.description}
+          onChangeText={description => this.setState({ description })}
+        />
+        <TouchableOpacity style={styles.searchButton} onPress={() => {}}>
+          <Text style={styles.searchButtonText}>Buscar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#c5d0dc',
-        paddingHorizontal: 20,
-        paddingTop: 30
-    },
-    input:{
-        borderRadius: 4,
-        borderWidth: 1,
-        borderColor: '#ddd',
-        padding: 15,
-        marginTop: 10,
-        fontSize: 16
-    },
-    searchButton: {
-        backgroundColor: '#7159C1',
-        borderRadius: 4,
-        heigth: 42,
-        marginTop: 15,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    searchButtonText:{
-        fontWeight: 'bold',
-        fontSize: 16,
-        color: '#FFF',
-    },
-
-     
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#F8F8FF",
+    paddingHorizontal: 20,
+    paddingTop: 30
+  },
+  input: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: 15,
+    marginTop: 10,
+    fontSize: 16
+  },
+  searchButton: {
+    backgroundColor: "#00FF7F",
+    borderRadius: 4,
+    height: 42,
+    marginTop: 15,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  searchButtonText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#FFF"
+  },
+  logo: {
+    height: 128,
+    width: 138,
+    resizeMode: "center"
+  }
+});
