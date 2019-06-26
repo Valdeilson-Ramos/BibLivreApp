9643e76f25d1084f1fcb232c44b18c02902e4602
valdeilson <valdeilson.ramos@ifp.edu.br>
2019-06-26 16:38:27 -0300

    inclusão página de listagem de exemplar

---
+++ src/pages/SearchList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+
+export default class SearchList extends Component {
+    state = {
+        searchList: []
+    };
+
+
+}
